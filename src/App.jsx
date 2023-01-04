29f9dcaab3baba25071dd367c72b38765f028da1
Jaspreet Singh <harshitbhattal@gmail.com>
2023-01-04 23:06:03 +0530

    initilize

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () =>  (
-    <div className='bg-dimBlue w-1/2 overflow-hidden'>
+    <div className='bg-primary text-white w-full p-5 overflow-hidden'>
       <div>
         <h1>
           hello world
