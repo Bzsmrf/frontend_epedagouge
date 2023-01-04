450a39a740d0cabc0b704a5485efcc21403262c0
Bzsmrf <dozerzzzz1234@gmail.com>
2023-01-04 22:39:15 +0530

    added dependencies

---
+++ e_pedagouge/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () =>  (
+    <div className='bg-dimBlue w-1/2 overflow-hidden'>
+      <div>
+        <h1>
+          hello world
+        </h1>
+      </div>
+    </div>
+  );
+
+
+export default App
