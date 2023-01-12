2843d6815bb3279d6349a5f6e066b6052096c3a5
Bzsmrf <dozerzzzz1234@gmail.com>
2023-01-12 10:08:18 +0530

    ADDED HOME PAGE

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,23 @@
 import Navbar from "./Navbar";
-import Billing from "./Billing";
+
 import CardDeal from "./CardDeal";
-import Business from "./Business";
+import Courses from "./Courses";
 import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
-
+import RegistrationForm from './RegistrationForm'
 export {
   Navbar,
-  Billing,
   CardDeal,
-  Business,
+  Courses,
   Clients,
   CTA,
   Stats,
   Footer,
   Testimonials,
   Hero,
+  RegistrationForm,
 }; 
